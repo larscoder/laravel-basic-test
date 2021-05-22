86fac34899f205130a166c105aa81345db4870dd
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2021-05-22 13:04:18 -0500

    Agregamos prueba en caso que este vacio los Tags

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Tag;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testEmpty()
+    {
+        $this->get('/')
+            ->assertStatus(200)
+            ->assertSee('No hay etiquetas');
+    }
+}
