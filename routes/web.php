e55666ad536a26d6ee2a0f20bc02ad4dd1678556
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2021-05-22 19:48:56 -0500

    Agregamos Test para agregar un tag

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +19,5 @@ Route::get('/', function () {
         'tags' => App\Models\Tag::get()
     ]);
 });
+
+Route::post('tags', [TagController::class, 'store']);
