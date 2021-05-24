215b786998910f959a18e397536f5c1538adc5db
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2021-05-24 06:59:26 -0500

    Agregamos la visualización del proyecto.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,23 +3,38 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
         <title>Laravel</title>
     </head>
-    <body>
-        <h4>Listado de etiquetas</h4>
-        <table>
-            @forelse($tags as $tag)
-                <tr>
-                    <td>
-                        {{ $tag->name }}
-                    </td>
-                </tr>
-            @empty
-                <tr>
-                    <td>No hay etiquetas</td>
-                </tr>
-            @endforelse
-        </table>
+    <body class="bg-gray-200 py-10">
+        <div class="max-w-lg bg-white mx-auto p-5 rounded shadow">
+            <form action="tags" method="POST" class="flex mb-4">
+                @csrf
+                <input type="text" name="name" class="rounded-l bg-gray-200 w-full outline-none p-4" placeholder="Nueva etiqueta">
+                <input type="submit" value="Agregar" class="rounded-r px-8 bg-blue-500 text-white outline-none">
+            </form>
+            <h4 class="text-lg text-center mb-4">Listado de etiquetas</h4>
+            <table>
+                @forelse($tags as $tag)
+                    <tr>
+                        <td class="border px-4 py-2">
+                            {{ $tag->name }}
+                        </td>
+                        <td class="border px-4 py-2">
+                            <form action="tags/{{ $tag->id }}" method="POST">
+                                @csrf
+                                @method('delete')
+                                <input type="submit" value="Eliminar" class="px-4 rounded bg-red-500 text-white outline-none">
+                            </form>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td>No hay etiquetas</td>
+                    </tr>
+                @endforelse
+            </table>
+        </div>
     </body>
 </html>
